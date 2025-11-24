19ba6b2ded1cade7d00a776c4c49680fff80148b
sachinppatange <info@spdesign.in>
2025-11-24 20:17:53 +0530

    Initial commit: project scaffold

--- test/adminpanel/admin_logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Admin logout page: destroys session and redirects to login
-require_once __DIR__ . '/admin_auth.php';
-
-admin_logout();
-
-// Redirect to admin login page
-header('Location: admin_login.php');
-exit;
